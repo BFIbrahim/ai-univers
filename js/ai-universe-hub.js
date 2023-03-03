90e8f21b4ef6b5a0325f9412abef01c79046bb43
BFIbrahim <ibrahim74543@gmail.com>
2023-03-03 03:44:16 -0800

    modal details content added

--- js/ai-universe-hub.js
+++ js/ai-universe-hub.js
@@ -20,9 +20,9 @@ const displayMeals = meals => {
           <h5 class="card-title">Features</h5>
           <p class="card-text">
           <ol>
-              <li>${data.features[0] ? data.features[0]:'No Features here'}</li>
-              <li>${data.features[1] ? data.features[1]:'No Features here'}</li>
-              <li>${data.features[2] ? data.features[2]:'No Features here'}</li>
+              <li>${data.features[0] ? data.features[0] : 'No Features here'}</li>
+              <li>${data.features[1] ? data.features[1] : 'No Features here'}</li>
+              <li>${data.features[2] ? data.features[2] : 'No Features here'}</li>
           </ol>
         </p>
         </div><hr>
@@ -63,12 +63,36 @@ const displayCardDetail = card => {
     detailCard.classList.add('col')
 
     detailCard.innerHTML = `
-        <div class="card">
+        <div class="card bg-danger bg-opacity-10">
         <div class="card-body">
             <h5 class="card-title">${card.description}</h5>
-            <p class="card-text">This is a longer card with supporting text below as a
-                natural lead-in to additional content. This content is a little bit longer.
-            </p>
+
+            <div class = "d-flex mx-auto">
+                <button class="btn btn-light text-success fw-medium me-3">${card.pricing[0].price} ${card.pricing[0].plan}</button>
+                <button class="btn btn-light text-warning fw-medium me-3">${card.pricing[1].price} ${card.pricing[0].plan}</button>
+                <button class="btn btn-light text-danger fw-medium me3">${card.pricing[2].price} ${card.pricing[0].plan}</button>
+            </div>
+
+            <div class = "d-flex justify-content-around mt-5">
+                <div>
+                    <h5>Features</h5>
+                    <ul>
+                        <li>${card.features[1].feature_name ? card.features[1].feature_name : 'No feature here'}</li>
+                        <li>${card.features[2].feature_name ? card.features[2].feature_name : 'No feature here'}</li>
+                        <li>${card.features[3].feature_name ? card.features[3].feature_name : 'No feature here'}</li>
+                    </ul>
+                </div>
+
+                <div>
+                    <h5>Integrations</h5>
+                    <ul>
+                        <li>${card.integrations[0] ? card.integrations[0] : 'No integrations'}</li>
+                        <li>${card.integrations[1] ? card.integrations[1] : 'No integrations'}</li>
+                        <li>${card.integrations[2] ? card.integrations[2] : 'No integrations'}</li>
+                    </ul>
+                </div>
+            </div>
+
         </div>
     </div>
 
@@ -82,23 +106,17 @@ const displayCardDetail = card => {
         <div class="card">
         <img src="${card.image_link[0]}" class="card-img-top" alt="...">
         <div class="card-body">
-            <h5 class="card-title"></h5>
-            <p class="card-text">This is a longer card with supporting text below as a
-                natural lead-in to additional content. This content is a little bit longer.
-            </p>
+            <h5 class="card-title">${card.input_output_examples[0].input ? card.input_output_examples[0].input : 'No question available here'}</h5>
+            
+            <p class="card-text">${card.input_output_examples[0].output ? card.input_output_examples[0].output : 'No Answer Found'}</p>
         </div>
     </div>
     `
 
     detailCardContainer.appendChild(detailImage)
-
-    // document.getElementById('cardDetail').innerText = card.description
-    // document.getElementById('cardDetailBody').innerHTML = `
-        
-    // `
 }
 
 getData()
 
 
-{/* <img src = "${card.image_link[0]}" class="img-fluid"/> */}
+{/* <img src = "${card.image_link[0]}" class="img-fluid"/> */ }
